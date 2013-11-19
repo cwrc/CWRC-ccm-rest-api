681b865f831a708b8e4bd83f941cb2020a32691a
adminuser <adminuser@ubuntu.(none)>
2013-11-19 14:55:41 -0800

    Added better url retrieval

--- util/sessionUtil.php
+++ util/sessionUtil.php
@@ -5,8 +5,8 @@ function cwrc_url() {
 }
 
 function cwrc_site() {
-	if(array_key_exists("REDIRECT_URL", $_SERVER)){
-		$pos = strpos($_SERVER["REQUEST_URI"], $_SERVER["REDIRECT_URL"]);
+	if(strlen($_SERVER[QUERY_STRING]) > 10){
+		$pos = strpos($_SERVER["REQUEST_URI"], substr($_SERVER["QUERY_STRING"], 10));
 		
 		if($pos == 0){
 			return "";
